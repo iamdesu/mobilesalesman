828457190d9219bfd839b8b44de59c4a498931ca
iamdesu <desu.sudarsana@gmail.com>
2015-04-05 09:38:54 +0800

    Initialization
    Project Structure

---
+++ app/src/main/java/com/bali/nusadua/productmonitor/AmbilDataActivity.java
@@ -0,0 +1,17 @@
+package com.bali.nusadua.productmonitor;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by desu sudarsana on 4/4/2015.
+ */
+public class AmbilDataActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ambil_data);
+    }
+
+}
