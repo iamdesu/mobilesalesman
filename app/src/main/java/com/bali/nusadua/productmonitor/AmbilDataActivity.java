962449caec64bbd5d1136ed9ad1f97b4bf56ce59
langit-p <LangitDaSilva.Putra@mitrais.com>
2015-04-10 18:18:36 +0800

    Add function to load data to spinner

--- app/src/main/java/com/bali/nusadua/productmonitor/AmbilDataActivity.java
+++ app/src/main/java/com/bali/nusadua/productmonitor/AmbilDataActivity.java
@@ -4,16 +4,34 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.bali.nusadua.productmonitor.com.bali.nusadua.productmonitor.adapter.SpinnerTeamAdapter;
+import com.bali.nusadua.productmonitor.com.bali.nusadua.productmonitor.model.Team;
+import com.bali.nusadua.productmonitor.com.bali.nusadua.productmonitor.model.TeamRepo;
+
+import java.util.List;
 
 /**
  * Created by desu sudarsana on 4/4/2015.
  */
-public class AmbilDataActivity extends Activity {
+public class AmbilDataActivity extends Activity implements AdapterView.OnItemSelectedListener{
+
+    Spinner mSpinnerTeam;
+    TextView mTvSelectedTeam;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ambil_data);
+        mTvSelectedTeam = (TextView) findViewById(R.id.tv_selected_team);
+        mSpinnerTeam = (Spinner) findViewById(R.id.spinnerTeam);
+        mSpinnerTeam.setOnItemSelectedListener(this);
+
+        insertDummyData();
+        loadTeam();
     }
 
     public void onBtnKeluarClick(View view) {
@@ -21,4 +39,40 @@ public class AmbilDataActivity extends Activity {
         startActivity(intent);
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        if(parent == findViewById(R.id.spinnerTeam)){
+            Team selectedTeam = (Team) parent.getItemAtPosition(position);
+            mTvSelectedTeam.setText(selectedTeam.getTeamName());
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        mTvSelectedTeam.setText("Not selected");
+    }
+
+    private void insertDummyData(){
+        TeamRepo repo = new TeamRepo(this);
+        repo.deleteAll();
+
+        for(int i = 0; i < 5; i++){
+            Team team = new Team();
+            team.setTeamId(i);
+            team.setTeamName("Team " + i);
+
+            repo.insert(team);
+        }
+    }
+
+    private void loadTeam(){
+        SpinnerTeamAdapter adapter = null;
+        TeamRepo repo = new TeamRepo(getApplicationContext());
+        List<Team> teams = repo.getAll();
+        adapter = new SpinnerTeamAdapter(AmbilDataActivity.this,
+                android.R.layout.simple_spinner_item, teams);
+        mSpinnerTeam.setAdapter(adapter);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    }
 }
