0e712805b1e81a2f7dadce75f68a8d4ca954ece2
langit-p <LangitDaSilva.Putra@mitrais.com>
2015-04-21 18:57:36 +0800

    Add function to upload to dropbox

--- app/src/main/java/com/bali/nusadua/productmonitor/MainActivity.java
+++ app/src/main/java/com/bali/nusadua/productmonitor/MainActivity.java
@@ -1,19 +1,68 @@
 package com.bali.nusadua.productmonitor;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.bali.nusadua.productmonitor.dropbox.UploadFileToDropbox;
+import com.dropbox.client2.DropboxAPI;
+import com.dropbox.client2.android.AndroidAuthSession;
+import com.dropbox.client2.session.AccessTokenPair;
+import com.dropbox.client2.session.AppKeyPair;
+import com.dropbox.client2.session.Session;
+import com.dropbox.client2.session.TokenPair;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private DropboxAPI<AndroidAuthSession> dropbox;
+    private final static String FILE_DIR = "/mobilesalesman/";
+    private final static String DROPBOX_NAME = "dropbox_prefs";
+    private final static String ACCESS_KEY = "xro5idej2ikdb4f";
+    private final static String ACCESS_SECRET = "k8egpv6pt3an92l";
+    private boolean isLoggedIn;
+    private Button mBtnKirimData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBtnKirimData = (Button) findViewById(R.id.btn_kirim_data);
+
+        //dipake buat autentikasi dropbox
+        loggedIn(false);
+        AndroidAuthSession session;
+        AppKeyPair pair = new AppKeyPair(ACCESS_KEY, ACCESS_SECRET);
+
+        SharedPreferences prefs = getSharedPreferences(DROPBOX_NAME, 0);
+        String key = prefs.getString(ACCESS_KEY, null);
+        String secret = prefs.getString(ACCESS_SECRET, null);
+
+        if (key != null && secret != null) {
+            AccessTokenPair token = new AccessTokenPair(key, secret);
+            session = new AndroidAuthSession(pair, Session.AccessType.APP_FOLDER, token);
+        } else {
+            session = new AndroidAuthSession(pair, Session.AccessType.APP_FOLDER);
+        }
+        dropbox = new DropboxAPI<AndroidAuthSession>(session);
+
+        loginDropBox();
+    }
+
+    private void loginDropBox(){
+            //buat log out drop box
+            dropbox.getSession().unlink();
+
+            //buat login dropbox
+            dropbox.getSession().startAuthentication(MainActivity.this);
+
     }
 
 
@@ -52,5 +101,44 @@ public class MainActivity extends ActionBarActivity {
     public void onTransaksiClick(View view) {
         Intent intent = new Intent(MainActivity.this, TransaksiActivity.class);
         startActivity(intent);
+    }
+
+    public void onButtonKirimDataClick(View view){
+        Log.i("Button ambil data", " Klik");
+        uploadFileToDropBox();
+    }
+
+    private void uploadFileToDropBox(){
+        loginDropBox();
+        UploadFileToDropbox upload = new UploadFileToDropbox(this, dropbox,
+                FILE_DIR);
+        upload.execute();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        AndroidAuthSession session = dropbox.getSession();
+        if (session.authenticationSuccessful()) {
+            try {
+                session.finishAuthentication();
+                TokenPair tokens = session.getAccessTokenPair();
+                SharedPreferences prefs = getSharedPreferences(DROPBOX_NAME, 0);
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putString(ACCESS_KEY, tokens.key);
+                editor.putString(ACCESS_SECRET, tokens.secret);
+                editor.commit();
+                loggedIn(true);
+            } catch (IllegalStateException e) {
+                Toast.makeText(this, "Error during Dropbox authentication",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public void loggedIn(boolean isLogged) {
+        isLoggedIn = isLogged;
+        mBtnKirimData.setEnabled(isLogged);
     }
 }
---
+++ app/src/main/java/com/bali/nusadua/productmonitor/dropbox/UploadFileToDropbox.java
@@ -0,0 +1,64 @@
+package com.bali.nusadua.productmonitor.dropbox;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import com.dropbox.client2.DropboxAPI;
+import com.dropbox.client2.exception.DropboxException;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Created by Langit_P on 4/21/2015.
+ */
+public class UploadFileToDropbox extends AsyncTask<Void, Void, Boolean> {
+    private DropboxAPI<?> dropbox;
+    private String path;
+    private Context context;
+
+    public UploadFileToDropbox(Context context, DropboxAPI<?> dropbox,
+                               String path) {
+        this.context = context.getApplicationContext();
+        this.dropbox = dropbox;
+        this.path = path;
+    }
+    @Override
+    protected Boolean doInBackground(Void... params) {
+        final File tempDir = context.getCacheDir();
+        File tempFile;
+        FileWriter fr;
+        try {
+            tempFile = File.createTempFile("file", ".txt", tempDir);
+            fr = new FileWriter(tempFile);
+            fr.write("Test upload files");
+            fr.close();
+
+            FileInputStream fileInputStream = new FileInputStream(tempFile);
+            dropbox.putFile(path + "textfile.txt", fileInputStream,
+                    tempFile.length(), null, null);
+            tempFile.delete();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (DropboxException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    @Override
+    protected void onPostExecute(Boolean result) {
+        if (result) {
+            Toast.makeText(context, "File Uploaded Sucesfully!",
+                    Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(context, "Failed to upload file", Toast.LENGTH_LONG)
+                    .show();
+        }
+    }
+}
